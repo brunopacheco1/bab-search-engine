8f4355e5ea03bb62763d98fa5fdf978d0598e07d
Bruno Pacheco <brunopacheco1@yahoo.com>
2017-02-23 21:18:51 -0300

    Ajustes.

--- src/main/java/com/engsoft29/bab/searchengine/crawler/UOLCrawler.java
+++
@@ -1,9 +0,0 @@
-package com.engsoft29.bab.searchengine.crawler;
-
-public class UOLCrawler extends BABCrawler {
-    
-    @Override
-    public String getSeed() {
-        return "https://noticias.uol.com.br/";
-    }
-}
